9984f7358e7f5e7fb7357028dcbfb5830514a19b
Drop Star <srq0309@outlook.com>
2018-09-09 22:45:27 +0800

    添加工具杂项模块utils

---
+++ include/utils/path_monitor.hpp
@@ -0,0 +1,15 @@
+﻿#ifndef PATH_MONITOR_HPP
+#define PATH_MONITOR_HPP
+
+/*!
+ * @file	ts_queue.hpp
+ *
+ * @author	梦丿无情
+ * @date	2018-09-09
+ *
+ * @brief   
+ */
+
+
+
+#endif // PATH_MONITOR_HPP
---
+++ src/utils/main.cpp
@@ -0,0 +1,6 @@
+#include <utils/path_monitor.hpp>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
